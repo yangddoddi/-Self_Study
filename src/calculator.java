c3e2d109712410a25ab0198294dec580273f2d41
eunchan <pud1251@gmail.com>
2022-05-27 21:30:34 +0900

    기본 변수 및 출력

--- src/calculator.java
+++ src/calculator.java
@@ -2,7 +2,7 @@ public class calculator {
 
     public static void main(String[] args) {
 
-        double valueOfSupply = 10000.0;
+        double valueOfSupply = 35000;
         double vat = valueOfSupply * 0.1;
         double retailPrice = valueOfSupply+vat;
         double costRate = valueOfSupply * 0.3;
@@ -11,5 +11,13 @@ public class calculator {
         double dividend2 = income * 0.3;
         double dividend3 = income * 0.2;
 
+        System.out.println("공급가 : " + valueOfSupply);
+        System.out.println("부가세 : " + vat);
+        System.out.println("소비자가 : " + retailPrice);
+        System.out.println("원가 : " + costRate);
+        System.out.println("순익 : " + income);
+        System.out.println("배당1 : " + dividend1);
+        System.out.println("배당2 : " + dividend2);
+        System.out.println("배당3 : " + dividend3);
     }
 }
