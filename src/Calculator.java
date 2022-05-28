17ef2795cc50e3b2821ecfe60edfa9dd5d4b2281
eunchan <pud1251@gmail.com>
2022-05-29 00:02:16 +0900

    인스턴스화

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,13 +1,48 @@
 import javax.swing.*;
 
 public class Calculator {
-    public static double valueOfSupply;
     public static double vatRate = 0.1;
     public static double expenseRate = 0.3;
 
     public static void main(String[] args) {
-        valueOfSupply = Double.parseDouble(JOptionPane.showInputDialog("공급가를 입력하세요"));
-        Fomula.formula.printResults();
+        Accounting storeA = new Accounting(10000);
+        storeA.printResults();
+
+        Accounting storeB = new Accounting(75000);
+        storeB.printResults();
     }
 }
 
+class Accounting {
+    public double valueOfSupply;
+    public Accounting (double valueOfSupply) {
+        this.valueOfSupply = valueOfSupply;
+    }
+    public double getVat() {
+        return this.valueOfSupply * Calculator.vatRate;
+    }
+    public double retailPrice() {
+        return this.valueOfSupply+ getVat();
+    }
+    public double costRate() {
+        return this.valueOfSupply * Calculator.expenseRate;
+    }
+    public double income() {
+        return this.valueOfSupply - costRate();
+    }
+    public double dividend(double magnification) {
+        return income() * magnification;
+    }
+    public void printResults() {
+        System.out.println("-------------------");
+        System.out.println("공급가 : " + this.valueOfSupply);
+        System.out.println("부가세 : " + getVat());
+        System.out.println("소비자가 : " + retailPrice());
+        System.out.println("원가 : " + costRate());
+        System.out.println("순익 : " + income());
+        System.out.println("배당1 : " + dividend(0.5));
+        System.out.println("배당2 : " + dividend(0.3));
+        System.out.println("배당3 : " + dividend(0.2));
+        System.out.println("-------------------");
+    }
+}
--- src/Fomula.java
+++ src/Fomula.java
@@ -1,29 +1,3 @@
 public class Fomula {
-    static class formula {
-        public static double getVat() {
-            return Calculator.valueOfSupply * Calculator.vatRate;
-        }
-        public static double retailPrice() {
-            return Calculator.valueOfSupply+getVat();
-        }
-        public static double costRate() {
-            return Calculator.valueOfSupply * Calculator.expenseRate;
-        }
-        public static double income() {
-            return Calculator.valueOfSupply - costRate();
-        }
-        public static double dividend(double magnification) {
-            return income() * magnification;
-        }
-        public static void printResults() {
-            System.out.println("공급가 : " + Calculator.valueOfSupply);
-            System.out.println("부가세 : " + getVat());
-            System.out.println("소비자가 : " + retailPrice());
-            System.out.println("원가 : " + costRate());
-            System.out.println("순익 : " + income());
-            System.out.println("배당1 : " + dividend(0.5));
-            System.out.println("배당2 : " + dividend(0.3));
-            System.out.println("배당3 : " + dividend(0.2));
-        }
+
     }
-}
