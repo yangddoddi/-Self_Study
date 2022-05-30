1653c98e6f7784c3f45c6f70b7d997a15800f13d
eunchan <pud1251@gmail.com>
2022-05-31 00:47:14 +0900

    다차원 배열 합계 구하기

---
+++ src/Array/doubleArray.java
@@ -0,0 +1,20 @@
+package Array;
+
+// 다차원 배열 합계 구하기
+public class doubleArray {
+    public static void main(String[] args) {
+    int[][] arr = { {1,2,3},
+            {4,5,6},
+            {7,8,9},
+            {10,11,12}};
+    int sum = 0;
+
+    for ( int[] outer : arr) {
+        for ( int inner : outer) {
+            sum += inner;
+        }
+        }
+
+    System.out.println(sum);
+    }
+}
