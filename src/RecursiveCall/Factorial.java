f4b154b3f508d5d2bb689b2dbbf57785d9a4ca0a
eunchan <pud1251@gmail.com>
2022-06-02 00:38:39 +0900

    재귀 factorial

---
+++ src/RecursiveCall/Factorial.java
@@ -0,0 +1,19 @@
+package RecursiveCall;
+
+public class Factorial {
+    static long calculate(long num) {
+        if (num == 0) return 0;
+        if (num == 1) return 1;
+        return num = num * calculate(--num);
+    }
+
+    public static void main(String[] args) {
+        long result = calculate(5);
+        System.out.println(result);
+
+        for (int i=0; i<21; i++) {
+            long result2 = calculate(i);
+            System.out.println(i + "! = " + result2);
+        }
+    }
+}
