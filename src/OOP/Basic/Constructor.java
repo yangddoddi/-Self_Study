966a6d82ec220db5556a8e16fb7f4070405cc9ff
eunchan <pud1251@gmail.com>
2022-06-03 02:36:07 +0900

    생성자 예제 기초 + 초기화 블럭

--- src/OOP/Basic/Constructor.java
+++ src/OOP/Basic/Constructor.java
@@ -13,6 +13,13 @@ class Dog {
 class Cat {
     String name;
     int age;
+    String species;
+    {
+        System.out.println("나는 인스턴스 초기화 블럭이다!");
+        species = "korean show hair";
+    } // 인스턴스 초기화 블럭
+    // 클래스가 생성될 때 한 번 수행된다.
+    // 생성자에 공통된 내용이 있을 때 주로 사용한다.
     Cat () {
         this("돌돌이", 12);
     } // 매개변수가 없을 시 기본값
@@ -35,15 +42,20 @@ class Mongoose {
 }
 
 public class Constructor {
+    static { System.out.println("나는 클래스 초기화 블럭이다!"); }
+
     public static void main(String[] args) {
         Dog mongmong = new Dog("돌돌이", 9);
         Cat meow = new Cat();
+        Cat meowmeow = new Cat("야옹이", 3);
+        // 인스턴스를 생성할 때 "나는 인스턴스 초기화 블럭이다!" 가 실행됨.
         Mongoose animal1 = new Mongoose("바보", 12);
         Mongoose animal2 = animal1;
 
-        System.out.println("멍멍이 이름 : " + mongmong.name + "멍멍이 나이 : " + mongmong.age );
-        System.out.println("고양이 이름 : " + meow.name + "고양이 나이 : " + meow.age );
-        System.out.println("몽구스 이름 : " + animal1.name + "몽구스 나이 : " + animal1.age );
-        System.out.println("복제 몽구스 이름 : " + animal2.name + "복제 멍멍이 나이 : " + animal2.age );
+        System.out.println("멍멍이 이름 : " + mongmong.name + ", 멍멍이 나이 : " + mongmong.age );
+        System.out.println("고양이 이름 : " + meow.name + ", 고양이 나이 : " + meow.age + ", 품종 : "+ meow.species  );
+        System.out.println("고양이 이름 : " + meowmeow.name + ", 고양이 나이 : " + meowmeow.age + ", 품종 : "+ meowmeow.species  );
+        System.out.println("몽구스 이름 : " + animal1.name + ", 몽구스 나이 : " + animal1.age );
+        System.out.println("복제 몽구스 이름 : " + animal2.name + ", 복제 멍멍이 나이 : " + animal2.age );
     }
 }
