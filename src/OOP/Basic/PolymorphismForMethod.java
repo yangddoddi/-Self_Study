b95bbfa9f40384af8eb434872fd9ca9ba658e167
eunchan <pud1251@gmail.com>
2022-06-06 01:53:01 +0900

    다형성 활용 + ArrayList

--- src/OOP/Basic/PolymorphismForMethod.java
+++ src/OOP/Basic/PolymorphismForMethod.java
@@ -1,5 +1,9 @@
 package OOP.Basic;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Vector;
+
 // 다형성을 활용하여 메서드를 일일히 하드코딩하지 않고 Product 타입을 적용함으로써 보다 간결하게 구현할 수 있다.
 class Product {
     final int price;
@@ -45,22 +49,45 @@ class Person {
         this.money = money;
     }
 
+    ArrayList<Product> shoppingList = new ArrayList<Product>(10);
+//  ArrayList의 메서드는 add(), remove(), isEmpty(), get(), size(), clear() 등이 있다.
+
     void buy(Product product) {
         if ( this.money + this.point < product.price ) {
-            System.out.println("금액이 부족합니다.");
+            System.out.println("buy() : " + "금액이 부족합니다.");
         } else if ( this.money < product.price && this.money + this.point >= product.price ) {
             this.money = this.money - product.price + this.point;
             this.point = product.point;
-            System.out.println(product + "를 구매했습니다. 남은 소지금은 " + this.money + "이며, 적립된 포인트는 " + product.point + "입니다. 총 적립 포인트는 " + this.point + "입니다.");
+            shoppingList.add(product);
+            System.out.println("buy() : " + product + "를 구매했습니다. 남은 소지금은 " + this.money + "이며, 적립된 포인트는 " + product.point + "입니다. 총 적립 포인트는 " + this.point + "입니다.");
         } else {
             this.money = this.money - product.price;
             this.point += product.point;
-            System.out.println(product + "를 구매했습니다. 남은 소지금은 " + this.money + "이며, 적립된 포인트는 " + product.point + "입니다. 총 적립 포인트는 " + this.point + "입니다.");
+            shoppingList.add(product);
+            System.out.println("buy() : " + product + "를 구매했습니다. 남은 소지금은 " + this.money + "이며, 적립된 포인트는 " + product.point + "입니다. 총 적립 포인트는 " + this.point + "입니다.");
+        }
+    }
+
+    void refund(Product product) {
+        if(shoppingList.contains(product)) {
+            shoppingList.remove(product);
+            System.out.println("refund() : " + product + " 환불 완료!");
+            this.money += product.price;
+            this.point -= product.point;
+        } else {
+            System.out.println("refund() : 해당 물건을 구입하지 않았습니다!");
         }
     }
 
     void check() {
-        System.out.println("현재 소지금은 " + this.money + "입니다. 적립 포인트는 " + this.point + "입니다.");
+        System.out.println("check() : 현재 소지금은 " + this.money + "입니다. 적립 포인트는 " + this.point + "입니다.");
+        int total = 0;
+        String itemList = Arrays.toString(shoppingList.toArray());
+        for( Product product : shoppingList) {
+            total += product.price;
+        }
+        System.out.println("check() : 구매한 상품의 총 금액은 " + total + "이며, 구매한 상품의 총 개수는 " + shoppingList.size() + "입니다.");
+        System.out.println("check() : 지금까지 구매한 상품 목록은 " + itemList + "입니다.");
     }
 }
 
@@ -68,14 +95,19 @@ class Person {
 public class PolymorphismForMethod {
     public static void main(String[] args) {
         Product TV = new TV();
-        Product Computer = new TV();
+        Product Computer = new Computer();
         Product Macbook = new Macbook();
 
-        Person person = new Person(2000000);
+        Person person = new Person(5000000);
 
         person.buy(Macbook);
-        person.check();
-
         person.buy(TV);
+        System.out.println("==========================");
+        person.check();
+        System.out.println("==========================");
+        person.refund(Macbook);
+        person.refund(Computer);
+        System.out.println("==========================");
+        person.check();
     }
 }
