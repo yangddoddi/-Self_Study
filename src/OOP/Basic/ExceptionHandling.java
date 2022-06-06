c5ddb55f4b5315c8ccfeee0492f26679d6b6b032
eunchan <pud1251@gmail.com>
2022-06-06 23:29:48 +0900

    예외처리 기본 예제

--- src/OOP/Basic/ExceptionHandling.java
+++ src/OOP/Basic/ExceptionHandling.java
@@ -9,11 +9,11 @@ public class ExceptionHandling {
             int randomNum = (int)(Math.random()*10);
             try {
                 result = num / randomNum;
+                System.out.println(result);
             } catch (ArithmeticException e) {
                 System.out.println("0");
             } // 자바에서 정수를 0으로 나누는 것은 허용되지 않기 때문에 ArithmeticExeption을 발생시킨다.
-            // 따라서 이 경우 0을 출력하는 것으로 예외 처리를 함으로써 예외처리.
-            System.out.println(result);
+            // 따라서 이 경우 0을 출력하는 것으로 예외 처리.
         }
     }
 }
