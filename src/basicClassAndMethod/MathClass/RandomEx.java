500074433deec125a29525c7a86068d5c5446b6f
eunchan <pud1251@gmail.com>
2022-06-09 18:35:50 +0900

    난수를 활용한 간이 그래프 예제

--- src/basicClassAndMethod/MathClass/RandomEx.java
+++ src/basicClassAndMethod/MathClass/RandomEx.java
@@ -8,6 +8,7 @@ public class RandomEx {
                 int[] numArr = new int[100];
                 int[] counter = new int[10];
 
+                //1. 난수 생성해서 배열에 넣는다.
                 System.out.println("randomNum = ");
                 for ( int i=0; i<numArr.length; i++) {
                         int randomNum = (int)(Math.random() * 10);
@@ -23,8 +24,10 @@ public class RandomEx {
 //                                }
 //                        }
 //                }
-                for ( int i=0; i<numArr.length; i++) {
-                        counter[numArr[i]]++;
+
+                // 2. 해당 값을 counter의 각 인덱스에 넣음(인덱스와 numArr의 값이 동일하기 때문에 가능)
+                for (int j : numArr) {
+                        counter[j]++;
                 }
 
                 System.out.println(Arrays.toString(counter));
@@ -35,6 +38,7 @@ public class RandomEx {
 
         }
 
+        // 3. counter의 [i]인덱스 값을 받아서 해당 개수만큼 mark를 리턴한다.
         static String drawMarks(char mark, int counter) {
                 char[] drawing = new char[counter];
                 for ( int i=0; i< drawing.length; i++) {
