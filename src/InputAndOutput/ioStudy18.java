78653cccd5d3ad390fa74b69d066d300b5a68f80
eunchan <pud1251@gmail.com>
2022-06-21 04:42:05 +0900

    FilenameFilter, renameTo 예제

--- src/InputAndOutput/ioStudy18.java
+++ src/InputAndOutput/ioStudy18.java
@@ -1,7 +1,30 @@
 package InputAndOutput;
 
+import java.io.File;
+import java.io.FilenameFilter;
+
 public class ioStudy18 {
     public static void main(String[] args) {
 
+
+        String currDir = System.getProperty("user.dir");
+        File dir = new File(currDir);
+        final String pattern = "txt";
+
+        //FilenameFilter를 이용한 필터링
+        String[] files = dir.list(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.contains(pattern);
+            }
+        });
+
+        for (String str : files) {
+            System.out.println(str);
+        }
+
+        File file = new File("1.txt");
+        file.renameTo(new File(dir, "3.txt"));
+        // renameTo를 이용한 파일명 변경
     }
 }
