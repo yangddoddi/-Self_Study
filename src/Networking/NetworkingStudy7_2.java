c0deb1b41b7ff35d85930e127766825ae977ee8a
eunchan <pud1251@gmail.com>
2022-06-23 10:27:16 +0900

    TCP 소켓 프로밍을 활용한 n:n멀티 채팅

---
+++ src/Networking/NetworkingStudy7_2.java
@@ -0,0 +1,29 @@
+package Networking;
+
+import javax.swing.*;
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class  NetworkingStudy7_2 {
+    public static void main(String[] args) {
+        System.out.println("희망하는 닉네임을 입력해주세요.");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        String IP = "127.0.0.1";
+
+        try {
+            Socket socket = new Socket(IP,7777);
+
+            Thread sender = new Thread(new Sender2(socket, name));
+            Thread receiver = new Thread(new Receiver2(socket));
+
+            sender.start();
+            receiver.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
