5d84a66de982f1803ad0eb7b05d8d4219c920cd3
yang eun chan <97802103+yangddoddi@users.noreply.github.com>
2022-06-25 07:38:20 +0900

    [Bronze II] Title: 손익분기점, Time: 108 ms, Memory: 12828 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+        int C = in.nextInt();
+
+        if (B >= C) {
+            System.out.println("-1");
+        } else {
+            System.out.println(A / (C - B) + 1);
+        }
+    }
+}
