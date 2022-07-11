708ccc001834cd50c780f18d67d6c48f5c6b9ecc
eunchan <pud1251@gmail.com>
2022-07-11 17:44:20 +0900

    피보나치 수열 생성기2

--- src/javaPractice/Fibonacci.java
+++ src/javaPractice/Fibonacci.java
@@ -8,32 +8,38 @@ public class Fibonacci {
     // 메모이제이션으로 연산 속도 향상
     public static int[] memo;
     public static void main(String[] args) throws IOException {
-        // 1단계. 프로그램 시작을 알리는 출력문 선언
+        // 1단계 유저 입력값 받기
+        int inputNum = getUserInput();
+        memo = new int[inputNum];
+        // 3단계. 결과 및 안내문구 출력
+        printNumbers(inputNum);
+
+    }
+    
+    // 1. 유저 입력값 받기
+    private static int getUserInput() throws IOException {
         System.out.print("[안내]피보나치 수열 프로그램 시작.\n" +
                 "원하는 수열의 개수를 입력해 주세요 : ");
-        // 사용자의 입력값 받기.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int inputNum = Integer.parseInt(br.readLine());
-        memo = new int[inputNum];
-
-
-        // 3단계.
-        // 결과 출력을 위한 안내 문구 출력 입니다.
-        // 반복문을 통해 호출
-        System.out.println("[피보나치 수열 출력]");
-        for (int i=0; i<inputNum; i++) {
-            System.out.println(printFibonacci(i));
-        }
-        System.out.println("[안내]프로그램을 종료합니다.");
+        return inputNum;
     }
 
-    // 2단계. 피보나치 수열 재귀 메서드
-    public static int printFibonacci(int num) {
+    // 2단계. 피보나치 수열 계산
+    public static int fibonacci(int num) {
         if (num==0 || num==1) { return 1; }
         else if (memo[num]>0) {
             return memo[num]; }
         else {
-            return memo[num] = printFibonacci(num - 1) + printFibonacci(num - 2);
+            return memo[num] = fibonacci(num - 1) + fibonacci(num - 2);
         }
+    }
+
+    private static void printNumbers(int inputNum) {
+        System.out.println("[피보나치 수열 출력]");
+        for (int i = 0; i< inputNum; i++) {
+            System.out.println(fibonacci(i));
+        }
+        System.out.println("[안내]프로그램을 종료합니다.");
     }
 }
