cf8b082e6351f758d3c5c4f1058ccbbe7c915440
yang eun chan <97802103+yangddoddi@users.noreply.github.com>
2022-07-20 16:38:29 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -3,30 +3,26 @@ import java.util.*;
 class Solution {
     public static boolean[] isCompositeNum = new boolean[3001];
     public int solution(int[] nums) {
-        findCompositeNum();
         int length = nums.length;
         int result = 0;
         for (int i=0; i<length-2; i++) {
             for (int j=i+1; j<length-1; j++) {
                 for (int k=j+1; k<length; k++) {
                     int sum = nums[i]+nums[j]+nums[k];
-                    if (!isCompositeNum[sum]) {
-                        result+=1;
-                    }
+                    if (sum%2==0 || sum%3==0) {continue;}
+                    result += (isPrimeNum(sum)) ? 1 : 0;
                 }
             }
         }
         return result;
     }
+        
+
     
-    public static void findCompositeNum() {
-        isCompositeNum[0] = isCompositeNum[1] = true;
-        for (int i=2; i*i<=isCompositeNum.length; i++) {
-            if ( !isCompositeNum[i]) {
-            for (int j=i*i; j<=isCompositeNum.length; j+=i) {
-                isCompositeNum[j] = true;
-            }
-            }
+    public static boolean isPrimeNum(int num) {
+        for (int i=5; i*i<=num; i+=2) {
+            if (num%i==0) { return false; }
         }
+        return true;
     }
 }
