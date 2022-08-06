c419e3e70a540783ced40a07dbef6b219628e2e8
yang eun chan <97802103+yangddoddi@users.noreply.github.com>
2022-08-06 16:48:59 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 136 ms, Memory: 15760 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,11 +1,10 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
     final static int SIGN = 4; // 사인 종류
-    static int[] numArr; // 주어지는 숫자
-    static ArrayList<String> signList = new ArrayList<>(); // 사칙연산 기호 리스트
+    static int[] numArr; // 초기 입력값
+    static int[] signArr = new int[SIGN]; // 사인 배열
     static int min = Integer.MAX_VALUE; // 최소값 대입할 변수
     static int max = Integer.MIN_VALUE; // 최대값 대입할 변수
 
@@ -17,88 +16,51 @@ public class Main {
         st = new StringTokenizer(br.readLine(), " ");
         numArr = new int[T];
 
+        // 정수 입력
         for (int i = 0; i < T; i++) {
             numArr[i] = Integer.parseInt(st.nextToken());
         }
 
+        // 연산자 입력
         st = new StringTokenizer(br.readLine(), " ");
-        addSignList(st, signList);
+        for (int i = 0; i < SIGN; i++) {
+            signArr[i] = Integer.parseInt(st.nextToken());
+        }
 
-        ArrayList<String> temp = new ArrayList<>();
-        boolean[] visited = new boolean[signList.size()];
-
-        backtracking(0, temp, visited);
+        backtracking(numArr[0], 1);
 
         System.out.println(max);
         System.out.println(min);
     }
 
-    private static void backtracking(int v, ArrayList<String> temp, boolean[] visited) {
-        if (signList.size() == temp.size()) {
-            int result = numArr[0];
-            for (int i=0; i<temp.size(); i++) {
-                result = calculate(result, temp.get(i), numArr[i+1]);
-            }
-
-            if (result > max) {
-                max = result;
-            }
-
-            if ( result < min ) {
-                min = result;
-            }
-
+    // 백트래킹 메서드
+    private static void backtracking(int num, int index) {
+        if (index == numArr.length) {
+            max = Math.max(max, num);
+            min = Math.min(min, num);
             return;
         }
 
-        for (int i=0; i< signList.size(); i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                temp.add(signList.get(i));
-                backtracking(i, temp, visited);
-                visited[i] = false;
-                temp.remove(temp.size() - 1);
-            }
-        }
-    }
-
-    private static void addSignList(StringTokenizer st, ArrayList<String> signList) {
-        for (int i = 0; i < SIGN; i++) {
-            int token = Integer.parseInt(st.nextToken());
-
-            for (int j = 0; j < token; j++) {
+        for (int i=0; i<signArr.length; i++) {
+            if (signArr[i] > 0) {
+                signArr[i]--;
                 switch (i) {
-                    case 0:
-                        signList.add("+");
+                    case 0 :
+                        backtracking(num+numArr[index], index+1);
                         break;
-                    case 1:
-                        signList.add("-");
+                    case 1 :
+                        backtracking(num-numArr[index], index+1);
                         break;
-                    case 2:
-                        signList.add("*");
+                    case 2 :
+                        backtracking(num*numArr[index], index+1);
                         break;
-                    case 3:
-                        signList.add("/");
+                    case 3 :
+                        backtracking(num/numArr[index], index+1);
                         break;
-                    default:
-                        throw new RuntimeException();
                 }
-            }
-        }
-    }
 
-    private static int calculate(int num1, String sign, int num2) {
-        switch (sign) {
-            case "+" :
-                return num1+num2;
-            case "-" :
-                return num1-num2;
-            case "*" :
-                return num1*num2;
-            case "/" :
-                return num1/num2;
-            default:
-                throw new RuntimeException();
+                signArr[i]++;
+            }
         }
     }
 }
