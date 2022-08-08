2a0af10ef39326703af1481718bb3c4d5d73dfe2
yang eun chan <97802103+yangddoddi@users.noreply.github.com>
2022-08-08 10:47:54 +0900

    [Bronze III] Title: 택시 기하학, Time: 76 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        double R = Integer.parseInt(br.readLine());
+
+        System.out.println(R * R * Math.PI);
+        System.out.println(R * R * 2);
+    }
+}
