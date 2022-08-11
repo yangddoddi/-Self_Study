3afc35e39bb14d678fa86b7c774f15525aed070d
yang eun chan <97802103+yangddoddi@users.noreply.github.com>
2022-08-11 12:46:11 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.66 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -3,8 +3,6 @@ import java.util.stream.*;
 
 class Solution {
     public int solution(int[] numbers) {
-    return IntStream.range(0,10)
-        .filter(num -> Arrays.stream(numbers).noneMatch(n->num==n))
-        .sum();
+    return 45 - Arrays.stream(numbers).sum();
     }
 }
