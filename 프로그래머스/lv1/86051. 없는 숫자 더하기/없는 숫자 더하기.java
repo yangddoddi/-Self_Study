2107b4214080d82900ec9c4bcb425def155feb75
yang eun chan <97802103+yangddoddi@users.noreply.github.com>
2022-08-11 12:01:40 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 2.53 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -2,16 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] numbers) {
-    ArrayList<Integer> numList = new ArrayList<>();
+    ArrayList<Integer> nums = new ArrayList<>();
     
-    for (int i=0; i<10; i++) {
-    numList.add(i);
+    for (int i=0; i<=9; i++) {
+        nums.add(i);
     }
-    
-    for (int n : numbers) {
-    numList.remove(Integer.valueOf(n));
-    }
-    
-    return numList.stream().mapToInt(m->m).sum();
+        
+    return nums.stream()
+        .filter(num -> Arrays.stream(numbers).noneMatch(n->n==num))
+        .mapToInt(m->m)
+        .sum();
     }
 }
