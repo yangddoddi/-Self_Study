3a3f2547873c395a9189c6277ccc7a7247b5a501
yang eun chan <97802103+yangddoddi@users.noreply.github.com>
2022-08-16 20:33:26 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.37 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,21 +1,62 @@
 class Solution {
     public String solution(int[] numbers, String hand) {
-        int left = 0;
-        int tight = 0;
+        int left = 10;
+        int right = 12;
         StringBuilder sb = new StringBuilder();
         
-        for (int n : numbers) {
+        for (int n : numbers) {
+            if ( n == 0 ) { n = 11; }
+            
             if ( n%3 == 1 ) {
                 left = n;
                 sb.append("L");
             }
+            
             if ( n%3 == 2 ) {
-                
+                if (getPosition(left,right,n).equals("LEFT")) {
+                    left = n;
+                    sb.append("L");
+                }
+                else if (getPosition(left,right,n).equals("RIGHT")) {
+                    right = n;
+                    sb.append("R");
+                }
+                else {
+                    if (hand.equals("left")) {
+                        left = n;
+                        sb.append("L");
+                    } else {
+                        right = n;
+                        sb.append("R");
+                    }
+                }
             }
+            
             if ( n%3 == 0 ) {
                 right = n;
                 sb.append("R");
             }
         }
+        
+        return sb.toString();
+    }
+    
+    public String getPosition(int left, int right, int num) {
+        
+        int leftX = (left-1)/3;
+        int leftY = (left-1)%3;
+        
+        int rightX = (right-1)/3;
+        int rightY = (right-1)%3;
+        
+        int numX = (num-1)/3;
+        int numY = (num-1)%3;
+        
+        int resultL = Math.abs(leftX-numX) + Math.abs(leftY-numY);
+        int resultR = Math.abs(rightX-numX) + Math.abs(rightY-numY);
+        
+        if (resultL == resultR) return "DRAW";
+        
+        return (resultL > resultR) ? "RIGHT" : "LEFT";
     }
 }
