8a9e89d951b07b3462116029d86ecca38a17d07e
yang eun chan <97802103+yangddoddi@users.noreply.github.com>
2022-08-24 11:30:40 +0900

    [level 1] Title: 신고 결과 받기, Time: 2.16 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/README.md
+++ 프로그래머스/lv1/92334. 신고 결과 받기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 신고 결과 받기 - 92334 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334) 
 
 ### 성능 요약
 
-메모리: 70.4 MB, 시간: 2.50 ms
+메모리: 79.8 MB, 시간: 2.16 ms
 
 ### 구분
 
--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -4,16 +4,6 @@ import java.util.stream.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        //id_list : 이용자 ID
-        // report : 신고한 이용자의 ID 정보 {이용자id 신고한id}
-        // k : 정지 횟수 k
-        // 반환값 : 이용자가 메일 받는 회수
-        
-        // 신고 당한 횟수는 map
-        
-        // report를 배열로 돌며 StringTokenizer로 문자열을 분리한다.
-        // 토큰 1번째는 신고자, 2번째는 신고 당한자
-        // 신고 당한자를 키, 신고한 자를 밸류로 넣음
         int[] answer = new int[id_list.length];
         Map<String, ArrayList<String>> result = new HashMap<>();
         Map<String, Integer> counter = new HashMap<>();
@@ -50,18 +40,5 @@ class Solution {
             }
         
         return answer;
-        
-
-        // 신고한 사람의 배열을 넣고
-        // 배열 꺼내서 돌려서 2 이상이면 숫자체크하면 되겠네요
-        
-        // 신고 횟수 자체는 제한 x
-        // 한 유저를 여러번 신고 가능하나 어차피 신고 횟수는 1회로 침
-        // k번 이상 신고되면 정지되고, 신고한 유저에게 메일 발송됨
-        
-        
-        // 신고한 유저 id , 유저가 신고한 id 기록
-        // 유저별 신고당한 횟수 저장
-        // 신고했는데 
     }
 }
