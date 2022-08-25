3b65c674f262566a7ed5372c931bb97f5e4d3c1b
yang eun chan <97802103+yangddoddi@users.noreply.github.com>
2022-08-25 20:26:03 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 31.70 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        LinkedList<Integer> list = new LinkedList<>();
+        
+        
+        for ( int n : arr ) {
+            if ( list.size() == 0 || list.peekLast() != n ) {
+            list.add(n);
+            }
+        }
+        
+        int[] answer = new int[list.size()];
+        
+        for ( int i=0; i<answer.length; i++ ) {
+            answer[i] = list.pollFirst();
+        }
+        
+        return answer;
+    }
+}
