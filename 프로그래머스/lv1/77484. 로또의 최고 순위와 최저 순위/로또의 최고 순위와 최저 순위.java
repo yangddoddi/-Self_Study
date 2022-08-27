44884fcee3ad95e7b69c99d2075b4df8ce8a1bad
yang eun chan <97802103+yangddoddi@users.noreply.github.com>
2022-08-27 13:42:46 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -3,24 +3,27 @@ import java.util.stream.*;
 
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        List<Integer> lottosList = Arrays.stream(lottos).boxed().collect(Collectors.toList());
-        List<Integer> winNumsList = Arrays.stream(win_nums).boxed().collect(Collectors.toList());
-        
-        winNumsList.retainAll(lottosList);
         
         int zero_count = 0;
+        int same_count = 0;
         
-        for (int i=0; i<lottosList.size(); i++) {
-            if (lottosList.get(i) == 0) zero_count++;
+        for (int lotto : lottos) {
+            if ( lotto == 0 ) zero_count++;
+            
+            for (int win_num : win_nums) {
+                if (win_num == lotto) same_count++;
+            }
         }
         
-        int highScore = 7 - (winNumsList.size() + zero_count);
-        int lowScore = 0;
+        int sum = zero_count+same_count;
         
-        if ( winNumsList.size() == 0 ) { lowScore = 6; }
-        if ( winNumsList.size() == 0 && zero_count == 0 ) { highScore = 6; }
-        if ( winNumsList.size() != 0 ) { lowScore = 7 - winNumsList.size(); }
+        int high_score = 7 - (same_count+zero_count);
+        int low_score = 7 - same_count;
         
-        return new int[]{highScore, lowScore};
+        if (high_score < 1) high_score = 1; 
+        if (high_score > 6) high_score = 6;
+        if (low_score > 6) low_score = 6;
+        
+        return new int[]{high_score, low_score};
     }
 }
