4917da1ae550ba718617d102f805fd9789df0ef5
yang eun chan <97802103+yangddoddi@users.noreply.github.com>
2022-09-13 11:48:03 +0900

    [Gold I] Title: 최솟값 찾기, Time: 2236 ms, Memory: 895028 KB -BaekjoonHub

--- 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.java
@@ -1,52 +1,44 @@
 import java.util.*;
 import java.io.*;
 
-
 public class Main {
-    public static void main (String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        /*
-        * 첫 번째 입력 : 숫자 개수 N과 임의 수 L
-        * 두 번째 입력 : 임의 수 배열
-        * Di = A(i-L+1) ~ Ai 중의 최소값
-        * D에 저장된 수를 고르시오
-        *
-        * */
+    public static void main(String[] args) throws IOException {
+        // 첫 번째 줄 숫자 개수 N, 임의 값 L
+        // 두 번째 줄 N개의 수 배열
 
+        //  i-L+1 => A(i-l+1) ~ Ai
+        // 입력값 처리 시작
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
         int N = Integer.parseInt(st.nextToken());
         int L = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine(), " ");
         int[] arr = new int[N];
 
+        st = new StringTokenizer(br.readLine(), " ");
         for (int i=0; i<N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        // 입력값 처리 끝
+        // 덱 생성
         Deque<Node> deque = new ArrayDeque<>();
         StringBuilder sb = new StringBuilder();
 
         for (int i=0; i<arr.length; i++) {
             Node node = new Node(i, arr[i]);
+            int min = i-L+1;
 
-//            if (deque.size() <= 1) {
-//                deque.add(node);
-//            }
-
-            while (!deque.isEmpty() && node.compareToNode(deque.getLast())) {
+            while (deque.size() != 0 && !node.compareToNode(deque.getLast())) {
                 deque.removeLast();
             }
 
-            deque.addLast(node);
-
-//            if (node.compareToNode(deque.getFirst())) {
-//                deque.removeFirst();
-//            }
+            deque.add(node);
 
             sb.append(deque.getFirst().getValue()).append(" ");
 
-            if (deque.getFirst().expired(i, L)) {
+            if (deque.getFirst().getIndex() == min) {
                 deque.removeFirst();
             }
         }
@@ -56,23 +48,23 @@ public class Main {
 }
 
 class Node {
-    private int index;
-    private int value;
+    private final int index;
+    private final int value;
 
-    public Node(int index, int num) {
+    public Node (int index, int value) {
         this.index = index;
-        this.value = num;
+        this.value = value;
+    }
+
+    public boolean compareToNode(Node node) {
+        return this.value > node.getValue();
     }
 
     public int getValue() {
         return this.value;
     }
 
-    public boolean compareToNode(Node node) {
-        return this.value < node.getValue();
-    }
-
-    public boolean expired(int i, int L) {
-        return this.index <= (i-L+1);
+    public int getIndex() {
+        return this.index;
     }
 }
