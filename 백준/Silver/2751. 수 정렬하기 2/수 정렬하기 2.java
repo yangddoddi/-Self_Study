b02ea957139fb10a696c9e42bea5d395b7c9a844
yang eun chan <97802103+yangddoddi@users.noreply.github.com>
2022-09-29 13:23:13 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 848 ms, Memory: 133132 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,26 +1,71 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
+
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        ArrayList<Integer> arr = new ArrayList<>();
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] temp = new int[N];
+
+        for (int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        mergeSort(arr, 0, arr.length-1, temp);
+
         StringBuilder sb = new StringBuilder();
+        for (int n : arr) {
+            sb.append(n).append("\n");
+        }
+        
+        System.out.println(sb.toString());
+    }
 
-        for (int i = 0; i < T; i++) {
-            arr.add(Integer.parseInt(br.readLine()));
+    private static void mergeSort(int[] arr, int left, int right, int[] temp) {
+        if (left == right) return;
+
+        int mid = (left+right) / 2;
+        mergeSort(arr, left, mid, temp);
+        mergeSort(arr, mid+1, right, temp);
+
+        sort(arr, left, right, mid, temp);
+    }
+
+    private static void sort(int[] arr, int low, int high, int mid, int[] temp) {
+        int l = low;
+        int r = mid+1;
+        int idx = low;
+
+        while (l <= mid && r <= high) {
+            if (arr[l] < arr[r]) {
+                temp[idx] = arr[l];
+                idx++;
+                l++;
+            } else {
+                temp[idx] = arr[r];
+                idx++;
+                r++;
+            }
         }
 
-        Collections.sort(arr);
-
-        for (int i = 0; i < arr.size(); i++) {
-            sb.append(arr.get(i)).append('\n');
+        if (l > mid) {
+            while(r <= high) {
+                temp[idx] = arr[r];
+                idx++;
+                r++;
+            }
+        } else {
+            while(l <= mid) {
+                temp[idx] = arr[l];
+                idx++;
+                l++;
+            }
         }
 
-        System.out.println(sb);
+        for(int i= low; i<=high; i++) {
+            arr[i] = temp[i];
+        }
     }
 }
