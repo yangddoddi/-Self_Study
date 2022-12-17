510f14ed41695cb46d5f07236ea8acd6b9b083b2
yang eun chan <97802103+yangddoddi@users.noreply.github.com>
2022-12-17 22:11:33 +0900

    [level 2] Title: H-Index, Time: 0.31 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -1,6 +1,26 @@
+import java.util.*;
+
 class Solution {
+// 발표 논문 중 h번 이상 인용된 논문이 h편 이상이고 나머지 논문이 h번 이하 인용되었으면 h 최대값이 h-in     
+// 1 <= 발표 논문 수 <= 1000
+// 0 <= 논문별 인용횟수 <= 10,000
     public int solution(int[] citations) {
+        Queue<Integer> queue = new PriorityQueue<>();
+        for (int citation : citations) {
+            queue.add(citation);
+        }
+        
         int answer = 0;
+        int temp = 0;
+        int count = 0;
+        while (!queue.isEmpty()) {
+            if (count > temp) temp = queue.poll();
+            
+            if (count <= temp && queue.size()+1 >= count) answer = count;
+            
+            if (count <= temp) count++;
+        }
+        
         return answer;
     }
 }
