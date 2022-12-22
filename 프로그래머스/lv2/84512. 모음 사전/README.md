64dd76a6abdb9f590c41818f8e1028552e142c4b
yang eun chan <97802103+yangddoddi@users.noreply.github.com>
2022-12-23 01:59:49 +0900

    [level 2] Title: 모음 사전, Time: 2.80 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/README.md
+++ 프로그래머스/lv2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512#) 
 
 ### 성능 요약
 
-메모리: 77.1 MB, 시간: 4.33 ms
+메모리: 74 MB, 시간: 2.80 ms
 
 ### 구분
 
--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -1,26 +1,26 @@
 import java.util.*;
 
 class Solution {
-    public static int temp = 0;
+    public static int answer = 0;
     // 조합 완전 탐색
     // A -> AA -> AAA -> AAAA -> AAAAA -> AAAAE -> AAAAI -> ..
     public int solution(String word) {
         String[] alphabet = new String[]{"A","E","I","O","U"};
         
         Set<String> set = new HashSet<>();
-        int answer = 0;
         for (int i=0; i<alphabet.length; i++) {
             dfs(set, alphabet, alphabet[i], word);
+            if (answer != 0) break;
         }
         
-        return temp;
+        return answer;
     }
     
     public void dfs(Set<String> set, String[] alphabet, String current, String word) {
         set.add(current);
         
         if (current.equals(word)) {
-            temp = set.size();
+            answer = set.size();
             return;
         }
         
