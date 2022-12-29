badcce9c8e45904100ec11b569ab5648e9a406f3
yang eun chan <97802103+yangddoddi@users.noreply.github.com>
2022-12-30 02:44:21 +0900

    [level 2] Title: 타겟 넘버, Time: 0.30 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,21 +1,6 @@
 class Solution {
-    private static int answer = 0;
-    
     public int solution(int[] numbers, int target) {
-        dfs(numbers, 0, 0, target);
-        
+        int answer = 0;
         return answer;
-    }
-    
-    private void dfs(int[] numbers, int curr, int idx, int target) {
-        if (idx == numbers.length) {
-            if (curr == target) { 
-                answer += 1;
-            }
-            return;
-        }
-        
-        dfs(numbers, curr, idx+1, target + numbers[idx]);
-        dfs(numbers, curr, idx+1, target - numbers[idx]);
     }
 }
