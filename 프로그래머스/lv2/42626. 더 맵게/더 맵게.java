a7926f2d9d108de5202433ed8a4a9d53dba4e3aa
yang eun chan <97802103+yangddoddi@users.noreply.github.com>
2023-01-05 02:02:16 +0900

    [level 2] Title: 더 맵게, Time: 1807.53 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -1,28 +1,26 @@
 import java.util.*;
 
 class Solution {
-    // 스코빌 지수가 가장 낮은 두 개의 음식을 섞음
-    // 섞을 스코빌 지수 = 가장 맵지 않은 스코빌 + (두 번째로 맵지 않은 스코빌 *2)
-    // K => 모든 음식 스코빌 지수가 K 이상이여야 함
     public int solution(int[] scoville, int K) {
-        int answer = 0;
+        Queue<Integer> queue = new PriorityQueue<>((e1, e2) -> e1 - e2);
         
-        Queue<Integer> queue = new PriorityQueue<>((o1, o2) -> o1 - o2);
         for (int s : scoville) {
             queue.add(s);
         }
         
-        while (queue.size() > 1) {
-            int min = queue.poll();
-            int next = queue.poll();
+        int answer = 0;
+        while (queue.peek() < K) {
+            int food = queue.poll();
             
-            int comb = min + (next * 2);
-            queue.add(comb);
-            answer++;
-            
-            if (queue.peek() > K) return answer;
+            if (food < K) {
+                if (queue.isEmpty()) return -1;
+                
+                int food2 = queue.poll();
+                queue.add(food + (food2 * 2));
+                answer++;
+            } 
         }
         
-        return -1;
+        return answer;
     }
 }
