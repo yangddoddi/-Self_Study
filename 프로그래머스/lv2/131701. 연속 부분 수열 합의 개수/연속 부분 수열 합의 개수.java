6b508caf86fc647cea9f607a9695278b1c53182d
yang eun chan <97802103+yangddoddi@users.noreply.github.com>
2023-01-12 20:27:51 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 814.26 ms, Memory: 119 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] elements) {
+        List<Integer> list = new ArrayList<>();
+        
+        for (int e : elements) {
+            list.add(e);
+        }
+        
+        for (int e : elements) {
+            list.add(e);
+        }
+        
+        Set<Integer> set = new HashSet<>();
+        int answer = 0;
+        int len = 1;
+        
+        while (len <= elements.length) {
+            
+            for (int i=0; i<elements.length; i++) {
+                int temp = list.get(i);
+                
+                for (int j=i+1; j<i+len; j++) {
+                    temp += list.get(j);
+                }
+
+                set.add(temp);
+            }
+            
+            len++;
+        }
+        
+        return set.size();
+    }
+}
