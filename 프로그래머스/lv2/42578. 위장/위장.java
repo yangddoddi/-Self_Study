12af5f5819f08067508bc61d8b8f466c7af53327
yang eun chan <97802103+yangddoddi@users.noreply.github.com>
2023-02-01 12:24:11 +0900

    [level 2] Title: 위장, Time: 1.20 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.java
+++ 프로그래머스/lv2/42578. 위장/위장.java
@@ -2,36 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, List<String>> map = new HashMap<>();
-        for (String[] c : clothes) {
-            map.put(c[1], new ArrayList());
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (int i=0; i < clothes.length; i++) {
+            String category = clothes[i][1];
+            map.put(category, map.getOrDefault(category, 0) + 1);
         }
         
-        for (String[] c : clothes) {
-            map.get(c[1]).add(c[0]);
-        }
-        
-        int values = map
-            .entrySet()
+        return map.entrySet()
             .stream()
-            .map(e -> map.get(e.getKey()).size())
-            .reduce(0, (e1, e2) -> e1+e2);
-        
-        long keys = map
-            .entrySet()
-            .stream()
-            .map(e -> e.getKey())
-            .count();
-        
-        int sum = map
-            .entrySet()
-            .stream()
-            .map(e -> map.get(e.getKey()).size())
-            .reduce(1, (e1, e2) -> e1*e2);
-        
-        System.out.println(values);
-        System.out.println(sum);
-        
-        return (keys == 1L) ? values : values + sum;
+            .map(e -> e.getValue())
+            .reduce(1, (a, b) -> (a) * (b+1)) - 1;
     }
 }
